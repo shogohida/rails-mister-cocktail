53bb6f50210f6932cb9f622403be986d663a4f67
Shogo Hida <shogo.hida@gmail.com>
2020-05-14 14:15:37 +0900

    cocktail app

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,6 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  # has_many create instance method, used always with plural
   validates :name, presence: true, uniqueness: true
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,10 +1,11 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+  # belongs_to creates an instance method
+  # variable dose.ingredient gives an instance @ingredient(reader, wrtier)
   validates :description, presence: true
   validates :cocktail, presence: true
   validates :ingredient, presence: true, uniqueness: { scope: :cocktail }
   # necessarry to validate cocktail and ingredient?
   # validates :cocktail, :ingredient, uniqueness: true
-  # unsure if this is a right way to write validation for pair
 end
