881dc0f671f09ac5c27a22740d5bc5debabaadc6
Shogo Hida <shogo.hida@gmail.com>
2020-09-24 23:18:14 +0900

    delegated ingredient to dose

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,7 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+  delegate :name, :to => :ingredient, :prefix => true
   # belongs_to creates an instance method
   # variable dose.ingredient gives an instance @ingredient(reader, wrtier)
   validates :description, presence: true
